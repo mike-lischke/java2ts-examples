fb0ee4cd364ef48fd3639af124b07d768e1f0de3
Mike Lischke <mike.lischke@oracle.com>
2023-04-01 19:26:16 +0200

    Settings + documentation

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# java2ts-demo
-A project to demonstrate the use of java2typescript.
+# java2ts-examples
+
+A repository containing a number of projects to convert Java code to TypeScript, using the java2typescript node package. The projects are of different complexity:
+
+* [simple](simple) - Shows the conversion of a simple Java class (an Enum) to TypeScript. It contains only the minimal required configuration settings.
+* [antlr4](antlr4) - A complex setup to convert the entire ANTLR4 Java runtime to Typescript. Being able to do that was the original idea of the java2typescript project. Like the simple setup it uses the `convert` script from java2typescript and a configuration file for conversion. This project requires a copy of the [ANTLR4 repository](https://github.com/antlr/antlr4) to be present in the same parent directory as this project.
+* [jdk-tests](jdk-tests) - This project does a custom setup for conversion. It does not use the `convert` script from java2typescript, but instead comes with an own script that allows to do more detailed setup. This project requires a copy of the [OpenJDK repository](https://github.com/openjdk/jdk) to be present in the same parent directory as this project.
+
+For each project there's a script entry in package.json to allow running them with a single mouse click (when using VS Code). Alternatively you can run them from the command line, e.g.:
+
+    npm run simple
+
+Before running any of the projects, you need to install the dependencies:
+
+    npm install
+
+## Conversion
+
+None of the example conversions produces an error free result. There are simply too many conditions that influence the process. The most common error classes that can occur are:
+
+- **Incomplete Conversion**: The source file may contain code that is not supported by java2typescript.
+- **Bugs**: The conversion process may contain bugs.
+- **Boxing**: Auto boxing (converting a primitive type to an arbitrary object type) in TypeScript is not possible. For example the conversion of a string literal to `java.lang.String` is a manual process. For this reason you may get errors when string literals appear in Java code where `java.lang.String` is expected. In such cases you have to manually adjust the code to deal with this situation.
+- **Unsupported Features**: Certain constructs cannot be converted automatically (e.g. `this` calls, aka. explicit constructor invocation). The tool does as much as it can to convert the code, but some manual work is needed to make the code compiling and working.
+- **Linter Errors**: Many projects use a linter (like ESLint). Converted Java code often does not conform to popular linter formatting rules. It is recommended to allow formatting the generated code using your favorite linter (e.g. in VS Code) to fix the most common problems (e.g. indentation). It is possible to let the tool add suppression commands for linter or spelling errors. See the `configuration.options.prefix` for a field to set.
---
+++ src/jdk-tests/convertJDKTests.ts
@@ -0,0 +1,150 @@
+/*
+ * This file is released under the MIT license.
+ * Copyright (c) 2021, 2023, Mike Lischke
+ *
+ * See LICENSE file for more info.
+ */
+
+/* cspell: ignore a4tstool */
+
+import path from "path";
+
+import {
+    IClassResolver, IConverterConfiguration, JavaToTypescriptConverter,
+} from "java2typescript";
+import { PackageSourceManager } from "../src/PackageSourceManager";
+
+/** Member sorting identifiers as used in the project's eslint configuration. */
+const memberOrderOptions = {
+    default: [
+        "signature",
+        "public-static-field",
+        "protected-static-field",
+        "private-static-field",
+        "public-decorated-field",
+        "protected-decorated-field",
+        "private-decorated-field",
+        "public-instance-field",
+        "protected-instance-field",
+        "private-instance-field",
+        "public-abstract-field",
+        "protected-abstract-field",
+        "public-field",
+        "protected-field",
+        "private-field",
+        "static-field",
+        "instance-field",
+        "abstract-field",
+        "decorated-field",
+        "field",
+        "public-constructor",
+        "protected-constructor",
+        "private-constructor",
+        "constructor",
+        "public-static-method",
+        "protected-static-method",
+        "private-static-method",
+        "public-decorated-method",
+        "protected-decorated-method",
+        "private-decorated-method",
+        "public-instance-method",
+        "protected-instance-method",
+        "private-instance-method",
+        "public-abstract-method",
+        "protected-abstract-method",
+    ],
+};
+
+const knownSDKPackages: string[] = [
+];
+
+const importResolver = (packageId: string): PackageSource | undefined => {
+    knownSDKPackages.forEach((value) => {
+        if (packageId === value) {
+            return PackageSourceManager.emptySource(value);
+        }
+    });
+
+    return undefined;
+};
+
+const classResolver = new Map<string, IClassResolver>([
+]);
+
+const convertJDKLangTests = async () => {
+    const antlrToolOptions: IConverterConfiguration = {
+        packageRoot: path.resolve(process.cwd(), "../jdk/test/jdk/java/lang"),
+        javaLib: "../../../../../../src",
+        include: [
+            "ImplicitStringConcatShapes.java",
+        ],
+        exclude: [
+            "AssertionError",
+            "ClassLoader",
+            "InheritableThreadLocal",
+            "IntegralPrimitiveToString.java",
+            "ModuleLayer",
+            "ModuleTests",
+            "Package",
+            "PrimitiveSumMinMaxTest.java",
+            "ProcessBuilder",
+            "ProcessHandle",
+            "Runtime",
+            "RuntimePermission",
+            "SecurityManager",
+            "StackWalker",
+            "StrictMath",
+            "System",
+            "Thread",
+            "ThreadGroup",
+            "ThreadLocal",
+            "WeakPairMap",
+            "annotation",
+            "instrument",
+            "invoke",
+            "management",
+            "module",
+            "ref",
+            "reflect",
+        ],
+        outputPath: "../jree/tests/jdk/java/lang",
+        options: {
+            importResolver,
+            classResolver,
+            convertAnnotations: false,
+            sourceMappings: [
+            ],
+            preferArrowFunctions: true,
+            autoAddBraces: true,
+            addIndexFiles: false,
+            addNullUnionType: false,
+            suppressTypeWithInitializer: true,
+            wrapStringLiterals: false,
+            memberOrderOptions,
+            prefix: `
+/* eslint-disable @typescript-eslint/prefer-for-of */
+/* eslint-disable max-len */
+/* cspell: disable */
+/* eslint-disable jsdoc/check-tag-names */
+`,
+        },
+        sourceReplace: new Map([
+        ]),
+
+        debug: {
+            pathForPosition: {
+                filePattern: "XXX",
+                position: {
+                    row: 120,
+                    column: 58,
+                },
+            },
+        },
+
+    };
+
+    const converter = new JavaToTypescriptConverter(antlrToolOptions);
+    await converter.startConversion();
+};
+
+await convertJDKLangTests();
